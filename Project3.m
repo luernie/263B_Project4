4464a2b507db1612222cd8ae0513bb83b42c1e3f
luernie <erniel1@uci.edu>
2025-02-25 16:26:48 -0800

    Final Version

--- Project3.m
+++ Project3.m
@@ -7,12 +7,12 @@ clear all; close all; clc
 % Setup
 sympref('FloatingPointOutput',true) % This allows floating point coefficient
 format short
-syms l1 l2 l3 t1 t2 t3 d4 dt1 dt2 dt3 dt4
+syms l1 l2 l3 t1 t2 t3 d4 dt1 dt2 dt3 dt4 d1 a2 a3
 
 % Given Robot Link Parameters
-d1 = 0.416; % m
-a2 = 0.325; % m
-a3 = 0.225; % m
+% d1 = 0.416; % m
+% a2 = 0.325; % m
+% a3 = 0.225; % m
 
 % DH parameters for robot
 alpha = [0  0   0   sym(pi)]; % Create a symbolic representation of a constant
@@ -58,11 +58,11 @@ v0 = [0;0;0];
 v1 = collect(R10*(cross(w0,P01)+v0),[dt1 dt2 dt3 dt4]); % This functions grabs the coefficient values for the dt1-dt4
 v2 = collect(R21*(cross(w1,P12)+v1),[dt1 dt2 dt3 dt4]);
 v3 = collect(R32*(cross(w2,P23)+v2),[dt1 dt2 dt3 dt4]);
-v4 = collect(R43*(cross(w3,P34)+v3),[dt1 dt2 dt3 dt4]);
+v4 = collect(R43*(cross(w3,P34)+v3)+[0;0;dt4],[dt1 dt2 dt3 dt4]);
 v4 = simplify(v4);
 % Derive our Jacobian Matrix
-[JV4] = equationsToMatrix([v4],[dt1 dt2 dt3]); % Make matrix form times the dt1-dt3 to get v4
-[JW4] = equationsToMatrix([w4],[dt1 dt2 dt3]);
+[JV4] = equationsToMatrix([v4],[dt1 dt2 dt3 dt4]); % Make matrix form times the dt1-dt3 to get v4
+[JW4] = equationsToMatrix([w4],[dt1 dt2 dt3 dt4]); %dt is the theta dot
 J4_VP = simplify([JV4;JW4]);
 % We are not done yet!!
 % Need to transform back to base frmae
@@ -71,87 +71,18 @@ JT = [R04, zeros(3,3);zeros(3,3), R04];
 % Derive Jacobian Matrix in Base Frame:
 J0_VP = simplify(JT*J4_VP)
 %% Lets plot the velocity ellipsoid
-close all; 
-qn = [0 pi/2 pi/2 0];
-Robot.plot(qn, 'workspace', [-1,1,-1,1,-1,1]);
-hold on
-Robot.vellipse(qn);
+%close all; 
+%qn = [0 pi/2 pi/2 0];
+%Robot.plot(qn, 'workspace', [-1,1,-1,1,-1,1]);
+%hold on
+%Robot.vellipse(qn);
 
 %% Explicit Method %%
 %% Forward Kinematics
-
 T03 = simplify(T01 * T12 * T23);
 T02 = simplify(T01 * T12);
 P02 = T02(1:3, 4);
 P03 = T03(1:3, 4);
-
-% syms t1 t2 t3 t4 t5 t6 a2 a3 d3 d4 
-% assume([t1 t2 t3 t4 t5 t6 a2 a3 d3 d4],'real')
-% 
-% % Using robotic toolbox to generate forward Kinematic will 
-% % Intruoduce many very small coefficients that cannot be cancelled to 0
-% % Lets use Matlab symbolic toolbox to eliminate those coefficients
-% T0_1 = [cos(t1) -sin(t1) 0 0;
-%         sin(t1) cos(t1) 0 0;
-%         0 0 1 0;
-%         0 0 0 1];
-% T1_2 = [cos(t2) -sin(t2) 0 0;
-%         0 0 1 0;
-%        -sin(t2) -cos(t2) 0 0;
-%         0 0 0 1];
-% T2_3 = [cos(t3) -sin(t3) 0 a2;
-%         sin(t3) cos(t3) 0 0;
-%         0 0 1 d3;
-%         0 0 0 1];
-% T3_4 = [cos(t4) -sin(t4) 0 a3;
-%         0 0 1 d4;
-%        -sin(t4) -cos(t4) 0 0;
-%         0 0 0 1];
-% T4_5 = [cos(t5) -sin(t5) 0 0;
-%         0 0 -1 0;
-%         sin(t5) cos(t5) 0 0;
-%         0 0 0 1];
-% T5_6 = [cos(t6) -sin(t6) 0 0;
-%         0 0 1 0;
-%        -sin(t6) -cos(t6) 0 0;
-%         0 0 0 1];
-% T0_6 = simplify(T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6);
-% T0_5 = simplify(T0_1 * T1_2 * T2_3 * T3_4 * T4_5);
-% T0_4 = simplify(T0_1 * T1_2 * T2_3 * T3_4);
-% T0_3 = simplify(T0_1 * T1_2 * T2_3);
-% T0_2 = simplify(T0_1 * T1_2);
-% % Rotational matrix
-% R0_1 = T0_1(1:3, 1:3);
-% R1_2 = T0_2(1:3, 1:3);
-% R0_2 = T0_2(1:3, 1:3);
-% R2_3 = T2_3(1:3, 1:3);
-% R0_3 = T0_3(1:3, 1:3);
-% R3_4 = T3_4(1:3, 1:3);
-% R0_4 = T0_4(1:3, 1:3);
-% R4_5 = T4_5(1:3, 1:3);
-% R0_5 = T0_5(1:3, 1:3);
-% R5_6 = T5_6(1:3, 1:3);
-% R0_6 = T0_6(1:3, 1:3);
-% 
-% R1_0 = transpose(R0_1);
-% R2_1 = transpose(R1_2);
-% R3_2 = transpose(R2_3);
-% R4_3 = transpose(R3_4);
-% R5_4 = transpose(R4_5);
-% R6_5 = transpose(R5_6);
-% 
-% % translational matrix [4 col of the transformation matrix]
-% P0_1 = T0_1(1:3, 4);
-% P1_2 = T0_2(1:3, 4);
-% P0_2 = T0_2(1:3, 4);
-% P2_3 = T0_3(1:3, 4);
-% P0_3 = T0_3(1:3, 4);
-% P3_4 = T0_4(1:3, 4);
-% P0_4 = T0_4(1:3, 4);
-% P4_5 = T0_5(1:3, 4);
-% P0_5 = T0_5(1:3, 4);
-% P5_6 = T0_6(1:3, 4);
-% P0_6 = T0_6(1:3, 4);
 %% Direct Differentiation
 % Follow the similar precedure with previous example [3 col of the transformation matrix]
 Z01 = T01(1:3, 3); % z-axis of frame 1 (Joint 1 axis)
@@ -162,17 +93,63 @@ Z04 = T04(1:3, 3); % z-axis of frame 4 (Joint 1 axis)
 % Z06 = T06(1:3, 3); % z-axis of frame 6 (Joint 1 axis)
 %%
 Pe = T04(1:3,4);
-J1 = [cross(Z01,(Pe-P01)); Z01]; 
+J1 = [cross(Z01,(Pe-P01)); Z01]; % Revolute Joint Equation
 J2 = [cross(Z02,(Pe-P02)); Z02]; 
 J3 = [cross(Z03,(Pe-P03)); Z03]; 
-J4 = [cross(Z04,(Pe-P04)); Z04]; 
+J4 = [Z04; [0; 0; 0;]]; % Prismatic Joints
 % J5 = [cross(Z05,(Pe-P05)); Z05]; 
 % J6 = [cross(Z06,(Pe-P06)); Z06]; 
 J_DD = simplify([J1, J2, J3, J4]);
 % det_J = (det(J_DD));
 
+
+%% Force Propogation %%
+syms fx fy fz nx ny nz 
+% See discussion notes on slide 26!
+f4 = [fx; fy; fz;];% Fill this part  Hint: translational speed at end effector was given as fx fy and fz
+f3 = R34*f4;% See slides for equation
+f3 = collect(f3,[fx fy fz]);% Collect and simplify the coefficients for further use
+f2 = R23*f3;% See slides for equation
+f2 = collect(f2,[fx fy fz]);% Collect and simplify the coefficients for further use
+f1 = R12*f2;% See slides for equation
+f1 = simplify(collect(f1,[fx fy fz]));% Collect and simplify the coefficients for further use
+
+% torque propagation
+n4 = [nx; ny; nz];% Fill this part  Hint: angular velocity at end effector was given as nx, ny and nz
+n3 = R34*n4+cross(P34,f3);% See slides for equation
+n3 = collect(n3,[fx fy fz nx ny nz]);% Collect and simplify the coefficients for further use
+n2 = R23*n3+cross(P23,f2);% See slides for equation
+n2 = collect(n2,[fx fy fz nx ny nz]);% Collect and simplify the coefficients for further use
+n1 = R12*n2+cross(P12,f1);% See slides for equation
+n1 = simplify(collect(n1,[fx fy fz nx ny nz]));% Collect and simplify the coefficients for further use
+
+% For revolute joints we only want the torques on z components
+tq1 = collect(transpose(n1)*[0; 0; 1;]);% Fill this part
+tq2 = collect(transpose(n2)*[0; 0; 1;]);% Fill this part
+tq3 = collect(transpose(n3)*[0; 0; 1;]);% Fill this part
+% For prsimatic joints, we only want forces on z directions as well
+tq4 = collect(transpose(f4)*[0; 0; 1;]);% pri
+
+%% Form Jacobian
+% Form the matrix by calling matlab function 'equationToMatrix'
+TQ1 = equationsToMatrix(tq1,[fx fy fz nx ny nz]); % Fill this part
+TQ2 = equationsToMatrix(tq2,[fx fy fz nx ny nz]); % Fill this part
+TQ3 = equationsToMatrix(tq3,[fx fy fz nx ny nz]); % Fill this part
+TQ4 = equationsToMatrix(tq4,[fx fy fz nx ny nz]); % Fill this part
+% Form the Jacobian Matrix using TQ1 through TQ4
+% Don't forget that we are using force propogation so we are deriving 
+% the transpose of the jacobian matrix
+J4_FP_T = [TQ1;TQ2;TQ3;TQ4;]; % Fill this part
+J4_FP = simplify(transpose(J4_FP_T)); % represented at end effector frame
+%% Transform to Base Frame
+% Now lets transform to base frame
+J_rotation = [R04 zeros(3,3); zeros(3,3) R04]; % Fill this part for Jacobian Rotational Matrix
+J0_FP = simplify(J_rotation * J4_FP);
+%% Calculate the Singularities
+simplify(det(J0_FP(1:2,1:2)));
 %%
 function [T, Tfinal] = FK(alpha, a, d, theta) % Calculated FK (263A Final Exam code)
+    syms d1 a2 a3
     % Initialization of the transformation from the base to the end effector
     T0_ee = eye(4);
     T = struct(); % Initialize structure array for T
